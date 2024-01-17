cee491e982338ff3bd433d071a08bf6f28f85aee
DinhSonFE <dinhson24072004@gmail.com>
2024-01-17 17:50:11 +0700

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Feed from "./components/Feed";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchFeed from "./components/SearchFeed";
 import VideoDetail from "./components/VideoDetail";
 import ChannelDetail from "./components/ChannelDetail";
@@ -7,8 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Feed />} />
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<Feed />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:channelId" element={<ChannelDetail />} />
